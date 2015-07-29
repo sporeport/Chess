f4233f120d30fb0ae9e658082a98f4465f4481e4
Sam Hone-Studer <swhstuder@gmail.com>
2015-07-29 17:48:07 -0400

    organize files into folders.

--- run_chess.rb
+++ run_chess.rb
@@ -1,20 +1,18 @@
 require 'colorize'
 
-require_relative 'errors.rb'
-require_relative 'piece.rb'
-require_relative 'stepping_piece.rb'
-require_relative 'sliding_piece.rb'
-require_relative 'rook.rb'
-require_relative 'queen.rb'
-require_relative 'bishop.rb'
-require_relative 'king.rb'
-require_relative 'knight.rb'
-require_relative 'pawn.rb'
-require_relative 'board.rb'
-require_relative 'human_player.rb'
-require_relative 'chess_game.rb'
-
-
+require './game_and_board/errors.rb'
+require './pieces/piece.rb'
+require './pieces/stepping_piece.rb'
+require './pieces/sliding_piece.rb'
+require './pieces/rook.rb'
+require './pieces/queen.rb'
+require './pieces/bishop.rb'
+require './pieces/king.rb'
+require './pieces/knight.rb'
+require './pieces/pawn.rb'
+require './game_and_board/board.rb'
+require './player/human_player.rb'
+require './game_and_board/chess_game.rb'
 
 if $PROGRAM_NAME == __FILE__
   player1 = HumanPlayer.new
